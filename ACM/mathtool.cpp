fae4023259744093a48b901563fdfa7a79992827
littlemm <785897146@qq.com>
2017-12-31 16:38:44 +0800

    添加项目文件。

---
+++ ACM/mathtool.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+#include "mathtool.h"
+
+
+using namespace std;
---
+++ ACM/mathtool.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include"stdafx.h"
+
+const int MAXN = 1010;
+const int MAXM = 1010;
+
+//template <class T>
+class Matrix {
+public:
+	int n, m;
+	int a[1010][1010];
+	Matrix(){}
+
+	//Matrix(int N, int M);
+	
+	void clear() { n = m = 0; memset(a, 0, sizeof(a)); }
+
+	/*Matrix operator -(const Matrix &b) const {
+		Matrix tmp;
+		tmp.n = n;
+		tmp.m = m;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				tmp.a[i][j] = a[i][j] - b.a[i][j];
+		return tmp;
+	}
+	Matrix operator +(const Matrix &b) const {
+		Matrix tmp;
+		tmp.n = n;
+		tmp.m = m;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				tmp.a[i][j] = a[i][j] + b.a[i][j];
+		return tmp;
+	}
+	Matrix operator *(const Matrix &b)const {
+		Matrix tmp;
+		tmp.n = n; tmp.m = m;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < b.m; j++)
+				for (int k = 0; k < m; k++)
+					tmp.a[i][j] += a[i][k] * b.a[k][j];
+		return tmp;
+	}
+	*/
+	//void Inverse();
+};
+
+
+
+/*
+Gauss 消元
+a:方程组矩阵
+n:未知数个数
+l,ans:储存解 ，l[]表示是否为自由元
+*/
+inline int Gauss(double a[][MAXN], bool l[], double ans[]);
